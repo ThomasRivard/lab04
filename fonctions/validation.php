6378531908aca51f5f363611e793f3027f9d49e5
Pier-Luc Brault <pier-luc@brault.me>
2025-03-25 17:46:25 -0400

    Retirer la solution

--- fonctions/validation.php
+++ fonctions/validation.php
@@ -1,18 +1,3 @@
 <?php
 
-function validerNombre($valeur, $min, $max) {
-    return is_numeric($valeur)
-        && $valeur >= $min
-        && $valeur <= $max;
-}
-
-function validerNombreEntier($valeur, $min, $max) {
-    return validerNombre($valeur, $min, $max)
-        && $valeur == (int)$valeur;
-}
-
-function validerValeurChoix($valeur, $choixPossibles) {
-    return in_array($valeur, $choixPossibles);
-}
-
 ?>
--- index.php
+++ index.php
@@ -38,40 +38,7 @@ if (isset($_GET['deconnecter'])) {
         afficherFormulaireAuthentification();
     } else if (isset($_GET['page'])) {
         /****** AJOUTEZ LE SWITCH CI-DESSOUS *******/
-        switch ($_GET['page']) {
-            case 'fibonacci':
-                require('pages/fibonacci.php');
-                break;
-            case 'fizzbuzz':
-                require('pages/fizzbuzz.php');
-                break;
-            case 'etoiles':
-                require('pages/etoiles.php');
-                break;
-            case 'citations':
-                require('pages/citations.php');
-                break;
-            case 'calculs':
-                require('pages/calculs.php');
-                break;
-            case 'conversion':
-                require('pages/conversion.php');
-                break;
-            case 'nombre-aleatoires':
-                require('pages/nombre-aleatoires.php');
-                break;
-            case 'filtre':
-                require('pages/filtre.php');
-                break;
-            case 'tri':
-                require('pages/tri.php');
-                break;
-            case 'calendrier':
-                require('pages/calendrier.php');
-                break;
-            default:
-                echo '<p>ERREUR: Page inexistante.</p>';
-        }
+        
     } else {
         echo '<h1>Laboratoire 04</h1>';
         afficherMenu();
