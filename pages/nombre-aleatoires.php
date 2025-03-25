bebe11ca6741243e10a8d4bd5c274387f7cd8267
Pier-Luc Brault <pier-luc@brault.me>
2025-03-25 09:55:57 -0400

    Solution nombres aléatoires

--- pages/nombre-aleatoires.php
+++ pages/nombre-aleatoires.php
@@ -1,7 +1,44 @@
 <?php
 
-// TODO: Fonction pour générer un tableau de X nombres aléatoires
+require_once('fonctions/validation.php');
+
+function genererNombresAleatoires($min, $max, $quantite) {
+    $nombres = [];
+    for ($i = 0; $i < $quantite; $i++) {
+        $nombres[] = rand($min, $max);
+    }
+    return $nombres;
+}
+
+function afficherNombres($nombres) {
+    echo '<ul>';
+    foreach ($nombres as $nombre) {
+        echo "<li>$nombre</li>";
+    }
+    echo '</ul>';
+}
+
+if (
+    isset($_POST['min']) && isset($_POST['max']) && isset($_POST['quantite'])
+    && validerNombreEntier($_POST['min'], 0, 1000000)
+    && validerNombreEntier($_POST['max'], 0, 1000000)
+    && $_POST['min'] < $_POST['max']
+    && validerNombreEntier($_POST['quantite'], 1, 100)
+) {
+    $nombres = genererNombresAleatoires($_POST['min'], $_POST['max'], $_POST['quantite']);
+    afficherNombres($nombres);
+}
 
 ?>
 
 <h1>Nombres aléatoires</h1>
+
+<form method="post">
+    <label for="min">Minimum:</label>
+    <input type="number" id="min" name="min" required>
+    <label for="max">Maximum:</label>
+    <input type="number" id="max" name="max" required>
+    <label for="quantite">Quantité:</label>
+    <input type="number" id="quantite" name="quantite" required>
+    <button type="submit">Générer</button>
+</form>
