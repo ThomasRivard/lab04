da82406cbfaa1659febced5956cd4ef2016205f3
Pier-Luc Brault <pier-luc@brault.me>
2025-03-24 23:01:17 -0400

    Solution Fibonacci

--- pages/fibonacci.php
+++ pages/fibonacci.php
@@ -1,1 +1,48 @@
+<?php
+
+function afficherFibonacci($nombre) {
+    $a = 0;
+    $b = 1;
+    $c = 0;
+
+    echo '<ol>';
+    for ($i = 0; $i < $nombre; $i++) {
+        echo "<li>$a</li>";
+        $c = $a + $b;
+        $a = $b;
+        $b = $c;
+    }
+    echo '</ol>';
+}
+
+function afficherFormulaire($nombreMax) {
+?>
+    <form method="post" action="?page=fibonacci">
+    <label for="nombre">Nombres à afficher:</label>
+    <select name="nombre">
+    <?php
+        for ($i = 1; $i <= $nombreMax; $i++) {
+            echo '<option value="' . $i . '">' . $i . '</option>';
+        }
+    ?>
+    </select>
+    <button type="submit">Afficher</button>
+<?php
+}
+
+?>
+
 <h1>Fibonacci</h1>
+
+<?php
+
+if (isset($_POST['nombre']) && (int)$_POST['nombre'] == $_POST['nombre']) {
+    afficherFibonacci($_POST['nombre']);
+} else {
+    afficherFibonacci(10);
+}
+
+
+afficherFormulaire(50);
+
+?>
